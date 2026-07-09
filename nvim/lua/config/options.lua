349603c943bb53d03ff40eafa95776889d6a7caf
mike <mike@local>
2026-07-08 20:22:18 -0400

    adding nvim config for nvim startup

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+
+vim.g.mapleader = " "
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 3
+vim.o.shiftwidth = 3
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.cursorline = true
+
+vim.cmd('syntax enable')
+
+vim.keymap.set('n', '<leader>e', ':Neotree<CR>')
+
